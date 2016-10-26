79ac60025fb139f4d25dc62ca4c77be17a117e3e
roe <kulakov@03.perm.ru>
2016-10-26 16:36:08 +0500

    kerb auth working1

--- ssowebservice/app/views.py
+++ ssowebservice/app/views.py
@@ -1,6 +1,21 @@
-from app import app
+from app import app, login_manager
 from flask import render_template, flash, redirect
+from flask_login import login_required, current_user
+from user import User
+
+@login_manager.request_loader
+def get_user(req):
+    uid = req.environ.get('REMOTE_USER')
+    user = User(uid)
+    return user
+        
 
 @app.route('/', methods = ['GET'])
+@login_required
 def home():
-    return render_template('home.html')
+    flash(current_user.get_id())
+    return render_template('base.html')
+    
+@app.route('/login')
+def login():
+    return 'login'
